68513bfffe7aaa1f33bfd24544fb67d5e5f0f44e
Aki <aki@example.com>
2025-07-12 11:01:24 +0900

    feat:　通貨換算ツールを円からドルに変換

--- python_practice/currency_converter.py
+++ python_practice/currency_converter.py
@@ -7,14 +7,15 @@ load_dotenv()
 
 api_key = os.getenv("EXCHANGERATE_API_KEY")
 
-# 日本円を取得するURL
-url = f'https://v6.exchangerate-api.com/v6/{api_key}/latest/JPY'
+# ドルを取得するURL
+url = f'https://v6.exchangerate-api.com/v6/{api_key}/latest/USD'
 
 # Making our request
 response = requests.get(url)
 data = response.json()
 df = json.loads(json.dumps(data))
 # JPYUSDのデータのみ抽出
-usd_rate = df['conversion_rates']['USD']
-print(f"日本円で１円は{usd_rate}ドルです")
+usd_rate = df['conversion_rates']['JPY']
+print(f"１ドルは{usd_rate}円です。")
+
 
