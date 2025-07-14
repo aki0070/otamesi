72b63f9cd2006762651addb781cd19d38bbacf82
Aki <aki@example.com>
2025-07-14 15:09:10 +0900

    feat: ループを復習するファイルを作成

---
+++ python_practice/practice04.py
@@ -0,0 +1,7 @@
+#　リストの要素を一つずつ取り出して表示
+fruits = ["apple", "banana", "cherry"]
+
+#　fruitsリストの各要素を、一つずつ fruit に代入しながら繰り返す
+for fruit in fruits:
+    print(f"私は{fruit}が好きです。")
+
---
+++ python_practice/practice05.py
@@ -0,0 +1,9 @@
+count = 0 #　初期値の設定
+
+while count < 3:
+    print(f"現在のカウント: {count}")
+    count = count + 1 #　繰り返しを終えるための条件：無限防止。
+
+print("ループが終了しました。")
+
+
