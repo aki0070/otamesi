b7827077dc523298925f4c5ef67f680b45dd183a
Aki <aki@example.com>
2025-07-15 18:27:06 +0900

    feat: ランダム生成を利用したスクリプトを作成

--- python_practice/chap3_5_2.py
+++
@@ -1,2 +0,0 @@
-for cnt in range(1,3,1):
-    print(cnt, "回目のハロー")
---
+++ python_practice/interactive_game.py
@@ -0,0 +1,25 @@
+import random #　乱数生成ライブラリ
+
+print("---数当てeeゲームへようこそ！---")
+print("１から10の数をe当ててみてね。")
+
+secret_number = random.randint(1,10)
+guess = 0 #　ユーザーの数字を初期化（初回は０aなので条件はTRUEになる）
+
+while guess != secret_number:
+    try:
+        guess_str = input("あんたの予想は？: ")
+        guess = int(guess_str)
+        
+        if guess < secret_number:
+            print("もっと大きいよ")
+        elif guess > secret_number:
+            print("もっと小さいよ")
+        else:
+            print(f"🎉正解！{secret_number}")
+    except ValueError:
+        print("それは数字じゃないよ！ 数字を入力してね。")
+    except Exception as e:
+        print(f"予期せぬエラーが発生しました: {e}")
+
+print("ゲーム終了！")
